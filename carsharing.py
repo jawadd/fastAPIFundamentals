089af7f6de615c4c8d3abe3dafc740946f0d42b6
Jawad Hussain <jawadduet@gmail.com>
2024-10-08 10:58:01 +0500

    Typed parameters

--- carsharing.py
+++ carsharing.py
@@ -4,14 +4,36 @@ from fastapi import FastAPI
 
 app = FastAPI()
 
+db = [
+    {"id": 1, "size": "s", "fuel": "gasoline", "doors": 3, "transmission": "auto"},
+    {"id": 2, "size": "s", "fuel": "electric", "doors": 3, "transmission": "auto"},
+    {"id": 3, "size": "s", "fuel": "gasoline", "doors": 5, "transmission": "manual"},
+    {"id": 4, "size": "m", "fuel": "electric", "doors": 3, "transmission": "auto"},
+    {"id": 5, "size": "m", "fuel": "hybrid", "doors": 5, "transmission": "auto"},
+    {"id": 6, "size": "m", "fuel": "gasoline", "doors": 5, "transmission": "manual"},
+    {"id": 7, "size": "l", "fuel": "diesel", "doors": 5, "transmission": "manual"},
+    {"id": 8, "size": "l", "fuel": "electric", "doors": 5, "transmission": "auto"},
+    {"id": 9, "size": "l", "fuel": "hybrid", "doors": 5, "transmission": "auto"}
+]
 
-@app.get("/")
-def welcome(name):
-    """Return a friendly welcome message."""
-    return {'message': f"Welcome, {name}, the Car Sharing service!"}
+
+@app.get("/api/cars")
+def get_cars(size: str|None = None, doors: int|None=None) ->list:
+    result=db
+    if size:
+        result= [car for car in db if car['size'] == size]
+    if doors:
+        result= [car for car in db if car['doors'] == doors]
+    return result
+
+# @app.get("/")
+# def welcome(name):
+#     """Return a friendly welcome message."""
+#     return {'message': f"Welcome, {name}, the Car Sharing service!"}
 
 
-@app.get("/date")
-def date():
-    """Return the current date/time."""
-    return {'date': datetime.now()}
+# @app.get("/date")
+# def date():
+#     """Return the current date/time."""
+#     return {'date': datetime.now()}
+
